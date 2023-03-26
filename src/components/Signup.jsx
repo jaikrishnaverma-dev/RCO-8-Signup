d3878f3227b81d6ac096152d603b54f3ed595367
jaikrishnaverma-dev <jaikrishnaverma@gmail.com>
2023-03-26 14:01:47 +0530

    final submit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import Helmet from "react-helmet";
 // initial state for state
 const initialState = {
   form: {
@@ -31,11 +30,13 @@ const Signup = () => {
   const captchaGenerator = () => {
     let uniqueChar = "";
     const randomChar =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      "ABCDEFGIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (let i = 0; i < 5; i++) {
       uniqueChar += randomChar.charAt(Math.random() * randomChar.length);
     }
-    setState({ ...state, captcha: uniqueChar });
+    setState((prev) => {
+      return { ...prev, captcha: uniqueChar };
+    });
   };
 
   // generate captcha when component mounted and when removed clear the ref
@@ -82,6 +83,10 @@ const Signup = () => {
   //  when form submitted then check all sides completed or not
   const submitHandler = (event) => {
     event.preventDefault();
+    if(!state.captchaVerified){
+      captchaMsg.current.innerText = "Invalid Captcha code";
+      captchaMsg.current.style.color = "red";
+    }
     let flag = true;
     Object.entries(state.form).forEach((key, val) => {
       if (val.msg) {
@@ -92,6 +97,7 @@ const Signup = () => {
     setState({ ...state, signupCompleted: flag });
   };
 
+
   //   when successfully signup
   if (state.signupCompleted)
     return (
@@ -99,11 +105,17 @@ const Signup = () => {
         <div className="container container--green">
           <div className="form">
             <h3>Signup Successfully</h3>
-            {Object.entries(state.form).map(([key, val]) => (
-              <p>{`${key.replace("_", " ")} : ${val.value}`}</p>
+            {Object.entries(state.form).map(([key, val],index) => (
+              <p key={`complete${index}`}>{`${key.replace("_", " ")} : ${val.value}`}</p>
             ))}
-            <button className="btn" onClick={() => setState(initialState)}>
-              Reset
+            <button
+              className="btn"
+              onClick={() => {
+                setTimeout(() => captchaGenerator(), 300);
+                setState({...state,signupCompleted:false,captchaVerified:false });
+              }}
+            >
+              Go Back
             </button>
           </div>
         </div>
@@ -114,17 +126,19 @@ const Signup = () => {
   return (
     <>
       <div className={`container ${darkMode ? "container--dark" : ""}`}>
+        
         <form className="form" onSubmit={submitHandler}>
-          <button
-            className="btn--switch"
-            onClick={() => setDarkMode((prev) => !darkMode)}
-          >
-            Switch to {darkMode ? "Light" : "Dark"} mode
-          </button>
+        <button
+          className="btn--switch"
+          type="button"
+          onClick={() => setDarkMode((prev) => !darkMode)}
+        >
+          Switch to {darkMode ? "Light" : "Dark"} mode
+        </button>
           <h2 className="form-heading">Sign Up Form</h2>
-          {Object.entries(state.form).map(([key, val]) => (
+          {Object.entries(state.form).map(([key, val],index) => (
             <>
-              <div className="input-wrapper">
+              <div key={`incomplete${index}`} className="input-wrapper">
                 <div className="input-icon">
                   <img
                     src={
